7418b7e8833e3a610d706924b9e5ed02b9af4e5b
Ali Azhari <aliazhari@gmail.com>
2026-09-10 15:47:50 -0400

    Creating Animal..

---
+++ 2026_09_10/Demo.java
@@ -0,0 +1,12 @@
+
+
+public class Demo {
+    
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        System.out.println("Animal Name: " + animal.getName());
+        System.out.println("Animal Age: " + animal.getAge());
+        animal.makeSound();
+    }
+
+}
